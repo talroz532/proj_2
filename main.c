118aefa1903e1f621442a96e4d1164d36e5109f1
talroz532 <67608539+talroz532@users.noreply.github.com>
2020-11-10 20:46:37 +0200

    Update main.c

--- main.c
+++ main.c
@@ -11,7 +11,6 @@ char grade[5];
 int div =0;
 double avg;
 
-
 do{
 printf("Enter your grade: ");
 scanf("%s", grade);
@@ -25,33 +24,21 @@ avg = sum/div;
 printf("\nYour average is: %.2f\n", avg);
 
 if(avg<=100 && avg>=90){
-
+        printf("Your letter grade is: A \n\n");
 }else if(avg <90 && avg>=80){
-printf("Your letter grade is: A \n\n");
-
+        printf("Your letter grade is: B \n\n");
 }else if(avg <80 && avg >=70){
-printf("Your letter grade is: B \n\n");
-
+        printf("Your letter grade is: C \n\n");
 }else if(avg <70 && avg >=60){
-printf("Your letter grade is: C \n\n");
-
+        printf("Your letter grade is: D \n\n");
 }else if(avg <60 && avg>=50){
-
-printf("Your letter grade is: D \n\n");
+        printf("Your letter grade is: E \n\n");
 }else if (avg <50 && avg >=0){
-
-printf("Your letter grade is: F\n\n");
+        printf("Your letter grade is: F\n\n");
 }else{
     printf("ERROR \n\n");
 }
-
-
-
 }while((strcmp(grade, "exit"))!=0);
-
-
-
-
 
     return 0;
 }
